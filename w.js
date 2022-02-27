b34f8d74b96bc71ff40047ee3cb9e9494f025e0e
omdropship <86162902+omdropship@users.noreply.github.com>
2022-02-27 18:14:01 +0700

    Create w.js

---
+++ w.js
@@ -0,0 +1,41 @@
+
+            <!-- &#9632;&#9632;&#9632;&#9632;&#9632; Info Toko & Produk &#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632; -->
+
+var tw_config = {
+'virtual_products': false,
+'marketplace': true,
+'category_home': true,
+'no_whatsapp': '0896-1888-5066',
+'no_telepon': '0896-1888-5066',
+'email': 'kangrian14@gmail.com',
+'pesan_salam': 'Halo',
+'nama_admin': 'Admin',
+'satuan': 'Pcs',
+'stock_habis': 'Habis',
+'FBPixel_ID': 123456789,
+};
+<!-- &#9632;&#9632;&#9632;&#9632;&#9632; Metode Pembayaran &#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632; -->
+
+var tw_payment = {
+'Transfer BCA' : '4371278065 A/n. Rian Nurherdian',
+'Paypal' : 'kangrian14@gmail.com',
+};
+
+<!-- &#9632;&#9632;&#9632;&#9632;&#9632; Market Place &#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632; -->
+
+var tw_marketplace = {
+'tokopedia' : 'https://www.tokopedia.com/URL-TOKO',
+'bukalapak' : 'https://www.bukalapak.com/u/URL-TOKO',
+'shopee' : 'https://shopee.co.id/URL-TOKO',
+'lazada' : 'https://www.lazada.co.id/shop/URL-TOKO',
+};
+
+<!-- &#9632;&#9632;&#9632;&#9632;&#9632; Media Sosial &#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632;&#9632; -->
+
+var tw_socmed = {
+'facebook' : 'https://www.facebook.com/kangrian.net/',
+'twitter' : 'https://www.twitter.com/kangriaan/',
+'instagram' : 'https://www.instagram.com/kangriaan/',
+'youtube' : 'https://www.youtube.com/kangrian?sub_confirmation=1',
+};
+          
